f7f2d2c45c68f315f70c2e49fafe827a952bb208
Bohdan Siriy <b.siriy@whirl.sg>
2017-09-11 18:00:30 +0300

    Build app

--- src/components/Preloader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Preloader() {
-  return (
-    <div>
-      <img src={require('../assets/img/giphy.gif')} alt="Data loading" />
-    </div>
-  )
-}
